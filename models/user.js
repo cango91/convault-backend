d010f004a42a63fae9810a20e181b72d0f108fd8
cango91 <can.gologlu@gmail.com>
2023-09-01 13:25:01 -0400

    Add User model

---
+++ models/user.js
@@ -0,0 +1,57 @@
+import * as mongoose from 'mongoose';
+
+function validateEmailPattern(val) {
+    return /^[^@\s]+@[^@\s]+\.[^@\s]+$/gi.test(val);
+}
+
+function usernameHasNoWhitespace(val) {
+    return !/\s/g.test(val);
+}
+
+function validatePasswordPattern(val) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\.\*!@_\-\(\)\[\]\=\?\'\"\\\/\#\$\%\|\^\&\+\:\;\!\<\>])[a-zA-Z\d\.\*!@_\-\(\)\[\]\=\?\'\"\\\/\#\$\%\|\^\&\+\:\;\!\<\>]{8,}$/.test(val);
+}
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: [usernameHasNoWhitespace, "{PATH} must not contain whitespace"]
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validateEmailPattern, "{PATH} does not match acceptable email pattern"],
+    },
+    password: {
+        type: String,
+        trim: true,
+        minLength: 8,
+        required: true,
+        validate: [validatePasswordPattern, "{PATH} does not meet minimum pattern requirements"]
+    },
+    salt: {
+        type: String,
+    },
+    publicKey: {
+        type: String,
+    },
+},
+    {
+        timestamps: true,
+        toJSON: {
+            transform: function (_, ret) {
+                delete ret.password;
+                delete ret.email;
+                delete ret.salt;
+                return ret;
+            }
+        }
+    });
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -7,17 +7,26 @@ const sanitize = require('express-mongo-sanitize');
 
 require('dotenv').config();
 
-const DEBUG = process.env.NODE_ENV && (['prod', 'production'].includes(process.env.NODE_ENV.toLowerCase()));
+const DEBUG = process.env.NODE_ENV ? (['prod', 'production'].includes(process.env.NODE_ENV.toLowerCase())) : true;
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(logger(DEBUG ? 'dev' : 'short'));
 app.use(express.json());
+// build folder is symlinked for conveneince during development
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(sanitize());
 app.use(cookieParser());
 
+// API ROUTES
+
+// "CATCH-ALL" ROUTE TO SERVE REACT-APP, MIGHT BE MIGRATED TO ANOTHER SERVER AND USE CORS
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Express app running on port ${port}`);
+    console.log(`Express app running on port ${PORT}`);
 });
